21f55aa743d73f17aeced8524dcbbc9cddff9964
Roshan Shrestha <shrestharoshan776@gmail.com>
2022-11-23 17:58:13 +0545

    Signup api added and database created

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,46 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 
-@Injectable({})
-export class AuthService{}
+
+@Injectable()
+export class AuthService {
+
+    constructor(private prisma: PrismaService) { }
+
+    async signup(dto: AuthDto) {
+
+        // generate the password hash
+        const hash = await argon.hash(dto.password);
+
+        try {
+            //save the new user in the db
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash,
+                }
+            });
+
+            delete user.hash;
+
+            //return the saved user
+
+            return user;
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException('Email already taken !')
+                }
+            }
+            throw error;
+        }
+
+    }
+
+    signin(dto: AuthDto) {
+        return { status: 200, message: 'Signin Successful !' }
+    }
+}
